3656ab53212fe70dd783ebbe4fae0fffabaadc65
Shafi Sourov <sourov.cse.brur@gmail.com>
2023-10-02 18:32:47 +0900

    update docker compose file

--- code/main.py
+++ code/main.py
@@ -8,20 +8,24 @@ from sklearn.preprocessing import StandardScaler
 import mlflow
 import os
 
-#initialize mlflow url and  experiment 
-mlflow.set_tracking_uri("https://mlflow.cs.ait.ac.th")
-mlflow.set_experiment(experiment_name="st124047-a3")
+# initialize mlflow url and  experiment 
+url = "https://mlflow.cs.ait.ac.th"
+experiment_name="st124047-a3"
 
-#Load Model
+mlflow.set_tracking_uri(url)
+mlflow.set_experiment(experiment_name)
+
+# #Load Model
 model_name = 'st124047-a3-model'
 model_version = 'Staging'
 loaded_model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{model_version}")
 
-# load the scaling parameters for both model(same scaler is used for the features for both models)
+
+# # load the scaling parameters for both model(same scaler is used for the features for both models)
 scaler_path = "scaler/scaler.pkl"
 loaded_scaler_params = pickle.load(open(scaler_path, 'rb'))
 
-# Create scaler with the loaded parameters
+# # Create scaler with the loaded parameters
 loaded_scaler = StandardScaler()
 loaded_scaler.mean_ = loaded_scaler_params['mean']
 loaded_scaler.scale_ = loaded_scaler_params['scale']
