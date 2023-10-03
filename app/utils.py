040c9bd3cefd770e7abc3c791ac5e6bb336c015d
Shafi Sourov <sourov.cse.brur@gmail.com>
2023-10-03 19:39:11 +0900

    Update sripts of yml

--- app/utils.py
+++ app/utils.py
@@ -13,19 +13,17 @@ mlflow.set_experiment(experiment_name)
 
 # #Load Model
 model_name = 'st124047-a3-model'
+# model_stage = 'Staging'
+
+# loaded_model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{model_stage}")
 
 try:
-    # Try to load the model from the 'Production' stage
+    # Try to load the model from the 'Staging' stage
     loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Staging")
 except mlflow.exceptions.MlflowException:
+    # Try to load the model from the 'Production' stage
+    loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Production")
 
-    try:
-        # Try to load the model from the 'Staging' stage
-        loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Production")
-    except mlflow.exceptions.MlflowException:
-        # Handle the case when the model is not found in either stage
-        print("Model not found in either 'Production' or 'Staging' stage.")
-        loaded_model = None  # or raise an exception or handle as needed
 
 # load the scaling parameters for both model(same scaler is used for the features for both models)
 scaler_path = "scaler/scaler.pkl"
