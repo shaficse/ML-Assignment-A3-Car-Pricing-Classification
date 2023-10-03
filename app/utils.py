7208a6201ac2465887f1d534dacfcde22915d16a
Shafi Sourov <sourov.cse.brur@gmail.com>
2023-10-03 19:23:23 +0900

    PassUnitTestTransiionBuildDeploy

--- app/utils.py
+++ app/utils.py
@@ -13,9 +13,19 @@ mlflow.set_experiment(experiment_name)
 
 # #Load Model
 model_name = 'st124047-a3-model'
-model_stage = 'Staging'
 
-loaded_model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{model_stage}")
+try:
+    # Try to load the model from the 'Production' stage
+    loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Staging")
+except mlflow.exceptions.MlflowException:
+
+    try:
+        # Try to load the model from the 'Staging' stage
+        loaded_model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/Production")
+    except mlflow.exceptions.MlflowException:
+        # Handle the case when the model is not found in either stage
+        print("Model not found in either 'Production' or 'Staging' stage.")
+        loaded_model = None  # or raise an exception or handle as needed
 
 # load the scaling parameters for both model(same scaler is used for the features for both models)
 scaler_path = "scaler/scaler.pkl"
@@ -24,15 +34,4 @@ loaded_scaler_params = pickle.load(open(scaler_path, 'rb'))
 # # Create scaler with the loaded parameters
 loaded_scaler = StandardScaler()
 loaded_scaler.mean_ = loaded_scaler_params['mean']
-loaded_scaler.scale_ = loaded_scaler_params['scale']
-
-def register_model_to_production():
-    from mlflow.client import MlflowClient
-    client = MlflowClient()
-    for model in client.get_registered_model(model_name).latest_versions: #type: ignore
-        # find model in Staging
-        if(model.current_stage == 'Staging'):
-            version = model.version
-            client.transition_model_version_stage(
-                name=model_name, version=version, stage="Production", archive_existing_versions=True
-            )          
+loaded_scaler.scale_ = loaded_scaler_params['scale']       
